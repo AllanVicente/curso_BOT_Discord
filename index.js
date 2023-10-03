b6ff960aae8bfa249972c44c38ad96c96da8c066
Allan <allaneduardo01@gmail.com>
2023-10-03 20:01:50 -0300

    adicionando o comando user

--- index.js
+++ index.js
@@ -1,55 +1,50 @@
-const {
-    Client,
-    Collection,
-    Events, 
-    GatewayCloseCodes,
-    GatewayIntentBits
-} = require('discord.js');
-
 const fs = require('node:fs');
 const path = require('node:path');
-
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds]//Guilds, forma como o discord chama os servidores
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands')
-const commandFiles = fs.readdirSync(commandsPath)
-.filter(file=>file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles){
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
 
-    if('data' in command && 'execute in command'){
-        client.commands.set(command.data.name, command);
-    }else{
-        console.log(`[WARNING] The command ar ${filePath} is missing a require " data" or "execute" property.`);
-    }
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
 }
 
-client.on(Events.InteractionCreate, async interection =>{
-    const command = interection.client.command.get(interection.commandName);
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
 
-    if(!command){
-        console.error(
-            `No command matching ${interection.commandName} was found.`
-        );
-    }
+  const command = interaction.client.commands.get(interaction.commandName);
 
-    try{
-        await command.execute(interection);
-    }catch(error){
-        console.error(error);
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
+  }
 });
 
-client.once(Events.ClientReady, c =>{
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.login(token);
